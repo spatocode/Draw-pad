25280326a36ac0165b0f99713f2601609b9f1fc0
Spatocode <impulseofficial069@gmail.com>
2015-11-18 13:22:33 +0100

    global variables

---
+++ scripts/draw.js
@@ -0,0 +1,8 @@
+var colorTool = document.querySelector('.colorbar');
+var drawSize = document.querySelector('.range');
+var output = document.querySelector('.output');
+var btn = document.querySelector('.button');
+var canvas = document.querySelector('canvas');
+var curX;
+var curY;
+var pressed = false;
